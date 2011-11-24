a89e65b8dbbd81283852afee88b7326a63d43146
Jeroen Dekkers <jeroen@dekkers.ch>
2011-11-24 21:45:07 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cube-routed',
+      version='1.0',
+      description='Cube-routed',
+      author='Dmitriy Samovskiy',
+      author_email='dmitriy.samovskiy@cohesiveft.com',
+      scripts=['cube-routed']
+     )
